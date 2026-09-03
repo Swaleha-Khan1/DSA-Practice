82a4c7ace8ff920b0c2c6feb4c8919c727916d90
Swaleha Khan <swalehakhan206@gmail.com>
2026-09-04 01:45:02 +0530

    Add Fibonacci solution

---
+++ dynamic_programming/fibonacci.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int fibo(int n, vector<int>& dp){
+    if(n<=1) return n;
+    if(dp[n]!=-1) return dp[n];
+
+    return dp[n] = fibo(n-1,dp)+fibo(n-2,dp);
+}
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    vector<int> dp(n+1,-1);
+    cout<<fibo(n,dp);
+    return 0;
+}
+
