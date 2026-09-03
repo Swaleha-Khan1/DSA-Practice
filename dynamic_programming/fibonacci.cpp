bd8bb76043c55b319ecfd80d880da1fc95ac13fa
Swaleha Khan <swalehakhan206@gmail.com>
2026-09-04 02:06:40 +0530

    Optimize solution

--- dynamic_programming/fibonacci.cpp
+++ dynamic_programming/fibonacci.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 using namespace std;
 
+// Memoization
 int fibo(int n, vector<int>& dp){
     if(n<=1) return n;
     if(dp[n]!=-1) return dp[n];
@@ -16,4 +17,50 @@ int main(){
     cout<<fibo(n,dp);
     return 0;
 }
+
+// Tabulation
+// int fibo(int n){
+//     if(n<=1) return n;
+//     vector<int> dp(n+1,-1);
+//     dp[0] = 0;
+//     dp[1] = 1;
+//     for(int i = 2; i<=n; i++){
+//         dp[i] = dp[i-1] + dp[i-2];
+//     }
+
+//     return dp[n];
+
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+
+//     cout<<fibo(n);
+//     return 0;
+// }
+
+// Space Optimization
+// int fibo(int n){
+//     if(n<=1) return n;
+//     int prev1 = 1;
+//     int prev2 = 0;
+//     for(int i = 2; i<=n; i++){
+//         int curr = prev1 + prev2;
+//         prev2 = prev1;
+//         prev1 = curr;
+//     }
+
+//     return prev1;
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+
+//     cout<<fibo(n);
+//     return 0;
+// }
 
